555d8a5616d31e074f99c019cf15e954aadfb38b
Anand O G <anandog12@gmail.com>
2026-02-23 20:46:48 +0530

    how to initialize a variable

--- app/src/main/java/com/example/learning_one/MainActivity.kt
+++ app/src/main/java/com/example/learning_one/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.learning_one
-
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -8,37 +9,43 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import com.example.learning_one.ui.theme.Learning_oneTheme
-import org.w3c.dom.Text
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-                   Greeting()
+                   MyCounterScreen()
                 }
             }
         }
 
 @Preview
 @Composable
-fun Greeting(){
+fun MyCounterScreen(){
+    var count by remember { mutableIntStateOf(0) }
     Column(
         modifier = Modifier.fillMaxSize(),
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Center,
         ) {
-        Text(text = "Hello World " , fontSize = 60.sp)
+        Text(text = "You Clicked $count times " ,
+            fontSize = 20.sp,
+            modifier = Modifier.padding(20.dp))
+        Button(onClick = { count++ })
+        {
+            Text("Click Me")
+        }
     }
 
 }
